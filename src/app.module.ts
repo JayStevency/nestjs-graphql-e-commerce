78226dc598756bcbac9266587efa9cbc351ceadc
jaystevency <yjy1129@kookmin.ac.kr>
2021-02-21 19:52:35 +0900

    chore: set up graphql on module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import GraphQLJSON from 'graphql-type-json';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmConfig } from './core/config/typeorm.config';
@@ -10,7 +13,21 @@ import { TypeOrmConfig } from './core/config/typeorm.config';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useClass: TypeOrmConfig
-    })
+    }),
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      resolvers: { JSON: GraphQLJSON },
+      definitions: { path: join(process.cwd(), 'src/graphql.ts') },
+      context: ({ req }) => ({
+        headers: req.headers,
+      }),
+      uploads: {
+        maxFileSize: 40000000,
+        maxFiles: 6,
+      },
+      playground: true,
+      debug: true
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
